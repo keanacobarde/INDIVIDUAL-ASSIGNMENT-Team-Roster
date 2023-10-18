415c44efaf97fbbf78a4311b0f0dc940ddd7fb0c
Keana Cobarde <keanacobarde@gmail.com>
2023-10-17 19:35:22 -0500

    reading member cards

--- components/MemberCard.js
+++ components/MemberCard.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import { Card, Button } from 'react-bootstrap';
+import { deleteMembers } from '../api/memberData';
 
-export default function MemberCard() {
+export default function MemberCard({ memberObj, onUpdate }) {
+  const deleteThisMember = () => {
+    if (window.confirm(`Delete ${memberObj.name}?`)) {
+      deleteMembers(memberObj.firebaseKey).then(() => onUpdate());
+    }
+  };
+
   return (
-    <div>MemberCard</div>
+    <Card style={{ width: '18rem', margin: '10px' }}>
+      <Card.Img variant="top" src={memberObj.image} alt={memberObj.name} style={{ height: '450px' }} />
+      <Card.Body>
+        <Card.Title>{memberObj.name}</Card.Title>
+        {/* DYNAMIC LINK TO EDIT THE BOOK DETAILS  */}
+        <p>{memberObj.role}</p>
+        <Link href={`/book/edit/${memberObj.firebaseKey}`} passHref>
+          <Button variant="info">EDIT</Button>
+        </Link>
+        <Button variant="danger" onClick={deleteThisMember} className="m-2">
+          DELETE
+        </Button>
+      </Card.Body>
+    </Card>
   );
 }
+
+MemberCard.propTypes = {
+  memberObj: PropTypes.shape({
+    firebaseKey: PropTypes.string,
+    image: PropTypes.string,
+    name: PropTypes.string,
+    role: PropTypes.string,
+    uid: PropTypes.string,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
--- pages/members.js
+++ pages/members.js
@@ -1,7 +1,29 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
+import { useAuth } from '../utils/context/authContext';
+import { getMembers } from '../api/memberData';
+import MemberCard from '../components/MemberCard';
 
-export default function members() {
+export default function Members() {
+  const [members, setMembers] = useState([]);
+
+  const { user } = useAuth();
+
+  const getAllTheMembers = () => {
+    getMembers(user.uid).then(setMembers);
+  };
+
+  useEffect(() => {
+    getAllTheMembers();
+  }, []);
+
   return (
-    <div>members</div>
+    <>
+      <div className="text-center my-4">
+        <div className="d-flex flex-wrap">
+          {members.map((member) => <MemberCard memberObj={member} onUpdate={getAllTheMembers} />)}
+        </div>
+      </div>
+    </>
   );
 }
